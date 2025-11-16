ad68886127b6303aaedcf63bc71a36b6ce5f9c29
r <martun5@icloud.com>
2025-11-16 11:50:56 +0200

    inital commit

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,10 @@
 package app
+
+import "github.com/rsmrtk/mybox/pkg"
+
+type App struct {
+	pkg *pkg.Facade
+
+	servicesGRPC *grpcservices.Service
+	servicesREST *restservices.Service
+}
--- pkg/pkg.go
+++ pkg/pkg.go
@@ -1,1 +1,11 @@
 package pkg
+
+import (
+	"github.com/rsmrtk/mybox/pkg/pkg_model"
+	lg "github.com/rsmrtk/smartlg"
+)
+
+type Facade struct {
+	Log *lg.Logger
+	M   *pkg_model.Models
+}
