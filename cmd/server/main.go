64f7c269838339ccf2354e06c891df75de1ffa32
r <martun5@icloud.com>
2025-11-16 12:38:14 +0200

    inital commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,21 +1,21 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
+import "github.com/rsmrtk/mybox/internal/app"
 
 func main() {
-	r := gin.Default()
-
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Congratulations!",
-		})
-	})
-
-	if err := r.Run(`:9595`); err != nil {
-		panic(err)
-	}
+	app.Run()
 }
+
+//func main() {
+//	r := gin.Default()
+//
+//	r.GET("/", func(c *gin.Context) {
+//		c.JSON(http.StatusOK, gin.H{
+//			"message": "Congratulations!",
+//		})
+//	})
+//
+//	if err := r.Run(`:9595`); err != nil {
+//		panic(err)
+//	}
+//}
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,10 +1,35 @@
 package app
 
-import "github.com/rsmrtk/mybox/pkg"
+import (
+	"context"
+	"os/signal"
+	"syscall"
+
+	"github.com/rsmrtk/mybox/pkg"
+)
 
 type App struct {
 	pkg *pkg.Facade
 
 	servicesGRPC *grpcservices.Service
 	servicesREST *restservices.Service
+}
+
+func Run() {
+	ctx, cancelFunc := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL)
+	defer cancelFunc()
+
+	app := new(App)
+
+	pkg, err := pkg.New(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	app.pkg = pkg
+	app.servicesGRPC = grpcservices.NewService(grpcservices.Options{Pkg: pkg})
+	app.servicesREST = restservices.NewService(restservices.Options{Pkg: pkg})
+
+	pkg.Log.Infof("Starting REST server...")
+
 }
