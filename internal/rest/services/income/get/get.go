7fcfa2b6c9e741f75ceea4373dca8d8f28fb73b8
r <martun5@icloud.com>
2025-11-17 11:39:45 +0200

    inital commit

--- internal/rest/services/income/get/get.go
+++ internal/rest/services/income/get/get.go
@@ -1,1 +1,28 @@
 package get
+
+import (
+	"context"
+
+	"github.com/rsmrtk/mybox/pkg"
+)
+
+type Facade struct {
+	pkg *pkg.Facade
+}
+
+func New(pkg *pkg.Facade) *Facade {
+	return &Facade{pkg: pkg}
+}
+
+func (f *Facade) Handler(ctx context.Context, req *income.GetRequest) error {
+	serv := &service{
+		ctx: ctx,
+		req: req,
+		f:   f,
+	}
+
+	if err := serv.find(); err != nil {
+		return nil, err
+	}
+
+}
