474cbed7cdb236668c7133dd50fb2c0300e01322
r <martun5@icloud.com>
2025-11-17 13:12:18 +0200

    inital commit

--- internal/rest/services/services.go
+++ internal/rest/services/services.go
@@ -1,1 +1,14 @@
 package services
+
+import (
+	"github.com/rsmrtk/mybox/internal/rest/services/income"
+	"github.com/rsmrtk/mybox/pkg"
+)
+
+type Options struct {
+	Pkg *pkg.Facade
+}
+
+type Services struct {
+	Income *income.Service
+}
